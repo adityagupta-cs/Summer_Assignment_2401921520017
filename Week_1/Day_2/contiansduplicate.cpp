dad1676a11f4bccc6179636f58f74123c7181336
Aditya Gupta <ag796653@gmail.com>
2026-06-02 18:31:41 +0530

    Added Week 1 Day 2 solutions

---
+++ Week_1/Day_2/contiansduplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+           if(freq[nums[i]]>=1){
+            return true;
+           }
+           freq[nums[i]]++;
+        }
+        return false;
+    }
+};
